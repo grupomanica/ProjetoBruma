a1148d304ef9887b5d2a5c2ced675ed6d3aa2ecd
Nicholly Gonzaga <nichollygonzaga@hotmail.com>
2026-04-09 20:21:44 -0300

    Update login-parceiro.php

--- PHP/login-parceiro.php
+++ PHP/login-parceiro.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- Bootstrap -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="../ASSETS/IMG/favicon/logo-iconeFullSize.png" type="image/x-icon">
 
-<!-- Ícones -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-<link rel="stylesheet" href="../CSS/login-parceiro.css">
+    <!-- Ícones -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
-<title>Bruma | Área da Clínica</title>
+    <link rel="stylesheet" href="../CSS/login-parceiro.css">
+
+    <title>Bruma | Área da Clínica</title>
 </head>
 
 <body>
@@ -24,11 +26,11 @@
 
 <div class="login-container">
 
-    <form class="login-form" action="processa_login_parceiro.php" method="POST">
+    <form class="login-form" action="" method="POST">
 
         <!-- LOGO -->
         <div class="text-center mb-3">
-            <img src="../Assets/logo2.png" width="70">
+             <img src="../ASSETS/IMG/logo-cNomeFullSize.png" width="100">
         </div>
 
         <h3 class="text-center mb-2">Área da clínica</h3>
