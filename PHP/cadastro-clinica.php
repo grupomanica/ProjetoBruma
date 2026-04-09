02c3cc06cdbc3605ff214915ed6cb9fd7b58cb30
Nicholly Gonzaga <nichollygonzaga@hotmail.com>
2026-04-09 20:20:36 -0300

    Update logo image path and favicon in cadastro-clinica.php

--- PHP/cadastro-clinica.php
+++ PHP/cadastro-clinica.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="shortcut icon" href="../ASSETS/IMG/favicon/logo-iconeFullSize.png" type="image/x-icon">
+
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -27,7 +29,7 @@
         <form class="cadastro-form" method="POST">
             <!-- LOGO -->
             <div class="text-center mb-3">
-                <img src="../Assets/logo2.png" width="70">
+                <img src="../ASSETS/IMG/logo-cNomeFullSize.png" width="100">
             </div>
 
             <h3 class="text-center mb-2">Cadastrar clínica</h3>
