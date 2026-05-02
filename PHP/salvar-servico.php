47a997eaaf8e831050144285a9c2f5de3b7c25af
MariMeMotta <memamotta777@gmail.com>
2026-05-02 00:52:24 -0300

    Atualização de selecionar serviços com BD

---
+++ PHP/salvar-servico.php
@@ -0,0 +1,21 @@
+<?php
+// SANITIZAÇÃO
+$servico = htmlspecialchars($_POST['servico'] ?? '');
+$clinica = htmlspecialchars($_POST['clinica'] ?? '');
+$endereco = htmlspecialchars($_POST['endereco'] ?? '');
+$valor = floatval($_POST['valor'] ?? 0);
+$data = htmlspecialchars($_POST['data'] ?? '');
+$hora = htmlspecialchars($_POST['hora'] ?? '');
+
+// VALIDAÇÃO
+if (!$servico || !$clinica || !$valor) {
+    die("Erro ao selecionar serviço");
+}
+
+$pix = "11999999999";
+
+// payload fake
+$payload = "BRUMA|$clinica|R$$valor|$data|$hora|$pix";
+
+$qrCodeUrl = "https://api.qrserver.com/v1/create-qr-code/?size=220x220&data=" . urlencode($payload);
+?>
