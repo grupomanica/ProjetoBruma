2a3d3847733777b99240c8cbe3e1dee0721683b5
proglogi <nichollygonzaga@hotmail.com>
2026-05-04 04:18:26 -0300

    Adicionando novos dados às tabelas e linhas de consulta

--- SQL/script.sql
+++ SQL/script.sql
@@ -14,26 +14,37 @@ CREATE TABLE usuarios (
 
 -- CLÍNICAS
 CREATE TABLE clinicas (
-    codigo INT PRIMARY KEY AUTO_INCREMENT ,
+    id INT PRIMARY KEY AUTO_INCREMENT ,
     nome VARCHAR(100) NOT NULL,
     cnpj VARCHAR(18) NOT NULL,
     cep VARCHAR(10) NOT NULL,
     cidade VARCHAR(50) NOT NULL,
     bairro VARCHAR(50) NOT NULL,
-    endereco VARCHAR(100) NOT NULL
+    endereco VARCHAR(100) NOT NULL,
+    logradouro VARCHAR(255),
+	faixa_preco VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    telefone VARCHAR(20),
+    senha VARCHAR(255)
 );
 
+ALTER TABLE clinicas 
+ADD senha VARCHAR(255);
   
 CREATE TABLE agendamentos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     data_agendamento DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-create table servicos
-( 
-botox char(30) not null
+
+CREATE TABLE servicos (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	botox char(30) not null
 );
 
 SELECT * FROM usuarios;
+SELECT * FROM clinicas;
+SELECT senha FROM clinicas;
 
-drop database sistemaBruma;
-drop table agendamento;
+DROP DATABASE sistemaBruma;
+DROP TABLE agendamento;
+DROP TABLE clinicas;
