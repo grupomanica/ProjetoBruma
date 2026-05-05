7589d21e98640573901161d7f3f969c3eb9abf9b
GuNunesB <gununes280@gmail.com>
2026-05-04 22:31:03 -0300

    Remoção de comentários

--- PHP/logout-clinica.php
+++ PHP/logout-clinica.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 
-// Remove todas as variáveis da sessão
 $_SESSION = [];
 
-// Remove o cookie da sessão (opcional, mas recomendado)
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
 
@@ -19,10 +17,8 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Destrói a sessão
 session_destroy();
 
-// Redireciona para login da clínica
 header("Location: login-clinica.php");
 exit();
 ?>
