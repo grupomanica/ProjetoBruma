ef2b7a1a75c78952a644adac99984847f78e21ed
proglogi <nichollygonzaga@hotmail.com>
2026-05-25 02:51:17 -0300

    Inserção do fav icon onde faltava

--- PHP/agendamento.php
+++ PHP/agendamento.php
@@ -73,6 +73,8 @@ $qrCodeUrl = "https://api.qrserver.com/v1/create-qr-code/?size=220x220&data=" . 
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<link rel="shortcut icon" href="ASSETS/IMG/favicon/logo-iconeFullSize.png" alt="logo" type="image/x-icon">
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
